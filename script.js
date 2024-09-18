82e26c34405a5154c868b635dc7ed8cf7a1ed792
Yun4g <delightvincent487@gmail.com>
2024-09-18 11:43:33 +0100

    comiit

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-let accordCont = document.querySelector(".accordion-item")
-console.log(accordCont);
- let btn = document.querySelector(".round-border" )
 
- btn.addEventListener('click', ()=>{
-    if(  accordCont.style.height = '65px') {
-        accordCont.style.height = '180px';
-    } 
-    
-    else{
-         accordCont.style.height = '65px';
-    }
-
- })
 
  
  
