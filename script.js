d526d6e3b62961cb126aaccb9a5086a31c3e87fb
Yun4g <delightvincent487@gmail.com>
2024-09-19 09:40:40 +0100

    up

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 console.log('hello world')
  const toggle = () =>{
     let menu = document.querySelector(".menu-btn_burger");
-
+    let link1 = document.querySelector(".links a");
+    let link = document.querySelector(".links2 a");
+    console.log(link)
     let toggle = document.querySelector(".toggle-menu");
 
     if (toggle.style.display === 'none' ||toggle.style.display === '') {
-        toggle.style.display = 'block'
+        toggle.style.display = 'block';
         setTimeout(()=>{
            toggle.style.height = '48vh'
         }, 10)
         menu.classList.add('open')
     }
     else{
-        toggle.style.height = '0vh'
         setTimeout(()=>{
-           toggle.style.display = 'none'
-        }, 1100)
+         toggle.style.display = 'none';
+       
+        }, 1000)
+
+        setTimeout(()=>{
+         toggle.style.height = '0vh'
+      }, 150)
         menu.classList.remove('open')
     }
    
