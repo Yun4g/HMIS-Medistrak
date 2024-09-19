447358aba0f76f16b3b467b1c1f05db3a6543c9c
Yun4g <delightvincent487@gmail.com>
2024-09-19 08:31:30 +0100

    up

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+console.log('hello world')
+ const toggle = () =>{
+    let menu = document.querySelector(".menu-btn_burger");
 
+    let toggle = document.querySelector(".toggle-menu");
+
+    if (toggle.style.display === 'none' ||toggle.style.display === '') {
+        toggle.style.display = 'block'
+        setTimeout(()=>{
+           toggle.style.height = '48vh'
+        }, 10)
+        menu.classList.add('open')
+    }
+    else{
+        toggle.style.height = '0vh'
+        setTimeout(()=>{
+           toggle.style.display = 'none'
+        }, 1100)
+        menu.classList.remove('open')
+    }
+   
 
  
+ }
  
